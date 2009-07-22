aeff90e1757ee173fb675a23c15eb28e5e22b761
Dan Wilson <dan@inkling-software.co.uk>
2009-07-22 17:12:59 +0100

    Search results POSTs to /graph, which creates a Graph instance.

--- mapping/urls.py
+++ mapping/urls.py
@@ -8,6 +8,11 @@ urlpatterns = patterns(
 )
 
 urlpatterns += patterns(
+    'mapping.views',
+    url(r'^graph/$', 'graph', name="graph"),
+)
+
+urlpatterns += patterns(
     'mapping.authviews',
     (r'^auth/$',                                    'coda_pre_auth'),
     (r'^auth/go/$',                                 'coda_auth'),
--- mapping/views.py
+++ mapping/views.py
@@ -1,16 +1,32 @@
 
 
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseNotAllowed
 from django.utils.encoding import DjangoUnicodeDecodeError
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django import forms
-
+                         
+from models import Graph
 
 @login_required
 def home(request):
     
     return render_to_response("home.html", {}, 
         context_instance=RequestContext(request))
+        
+@login_required
+def graph(request):
+    if request.method == "POST":
+        
+        graph = Graph()
+        graph.title = request.POST["title"]
+        graph.timetric_id = request.POST["id"]
+        graph.user = request.user
+        graph.save()
+        
+        return render_to_response("graph.html", {"pk":graph.pk}, 
+            context_instance=RequestContext(request))
+    else:
+        return HttpResponseNotAllowed(["POST",])
