f5854732142825e9e2a46a32a980639bd79fd547
Dan Wilson <dan@inkling-software.co.uk>
2009-07-22 17:16:40 +0100

    Tell vis/views.py to use Graph objects

--- visualisation/views.py
+++ visualisation/views.py
@@ -1,14 +1,15 @@
 # Create your views here.
 
-
 from django.shortcuts import render_to_response, get_object_or_404
+
+from mapping.models import Graph
 
 def standard_plot(request, mapping_id):
     
     # get mapping out the database here...
-    
+    g = Graph.objects.get(pk=int(mapping_id))
     return render_to_response('standardview.html', 
-        {'time_series_id': 'uMWN-IUdTBGLKc4VJmHoQA'})
+        {'time_series_id': g.timetric_id})
     
     
     
